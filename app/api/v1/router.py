fdc542bbdf858b4041dbde92486be2257f534eca
noah <d0hamzie@gmail.com>
2026-04-29 19:28:27 +0900

    KakaoMap API TEST

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.v1.endpoints import health
+from app.api.v1.endpoints import map
 from app.api.v1.endpoints import chat, extra # chat 라우터 추가
 
 api_router = APIRouter()
@@ -8,3 +9,4 @@ api_router.include_router(health.router, prefix="/health", tags=["health"])
 # 라우터 추가
 api_router.include_router(chat.router, prefix="/chat", tags=["Chat"])
 api_router.include_router(extra.router, prefix="/extra", tags=["extra-services"])
+api_router.include_router(map.router, prefix="/map", tags=["map"])
